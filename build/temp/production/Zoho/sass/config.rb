3ae634d2d764dec2c298310b52ec086f09544081
madhuchandrakanth94 <chandrakanth.madhu@walkingtree.tech>
2019-04-09 20:08:36 +0530

    Changed the view of AddContcs

---
+++ build/temp/production/Zoho/sass/config.rb
@@ -0,0 +1,1 @@
+cache_path = '/var/www/html/SenchaProject/build/.sass-cache'
--- packages/local/contacts/src/view/contacts/addcontact/AddContact.js
+++ packages/local/contacts/src/view/contacts/addcontact/AddContact.js
@@ -3,54 +3,52 @@ Ext.define('Contacts.view.contacts.addcontact.AddContact', {
 
   xtype: 'addcontact',
 
- 
-
-  // header: {
-  //   title: 'Contacts',
-  //   cls: 'addcontact-header'
-  // },
-
-  // cls: 'addcontact',
+  // layout: 'vbox',
 
   margin: '0 100px',
 
   defaults: {
     margin: '10px 0'
   },
-  
+
   items: [
     {
       xtype: 'panel',
       layout: 'hbox',
       // height: 100,
       items: [{
-        html: '<div style="padding-top: 10px;height: 100px;font-size: 42px;color: rgba(0, 0, 0, 1);font-family: "Muli", SemiBold;font-weight: bolder;">Contacts</div>'
-      },{
+        html: '<div style="padding-top: 10px;height: 50px;font-size: 42px;color: rgba(0, 0, 0, 1);font-family: "Muli", SemiBold;font-weight: bolder;">Contacts</div>'
+      }, {
         xtype: 'tbfill'
-      },{
+      }, {
         cls: 'addcontact-button',
-      xtype: 'button',
-      text: '+ Add Button',
+        xtype: 'button',
+        text: '+ Add Button',
       }]
     },
     {
       xtype: 'panel',
+      cls: 'addcontact',
       layout: {
         type: 'vbox',
-    
+
         pack: 'center',
-    
+
         align: 'middle',
       },
+
+      padding: '100 0',
+
       items: [{
+        padding: '0 0 30 0',
         html: '<div class="empty-text" style="text-align: center;color: rgb(180, 148, 148);font-family: "Muli", sans-serif;opacity: 0.53;">Your Contact List is empty! <br> Please add the contact by pressing add contact button.</div>'
-      },{
+      }, {
         cls: 'addcontact-button',
-      xtype: 'button',
-      text: '+ Add Button',
+        xtype: 'button',
+        text: '+ Add Button',
       }]
       // cls: 'empty-text',
-      
+
     }
   ]
 
