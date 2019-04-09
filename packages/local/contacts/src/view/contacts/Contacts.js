3a41ff4e0dc188a18be2d7ce01b85ef6fd5eb1a1
vickyraj430 <vicky.kumar@walkingtree.tech>
2019-04-09 19:36:07 +0530

    Added edit Contact folder

--- packages/local/contacts/src/view/contacts/Contacts.js
+++ packages/local/contacts/src/view/contacts/Contacts.js
@@ -1,27 +1,28 @@
 Ext.define('Contacts.view.contacts.Contacts',{
     extend: 'Ext.panel.Panel',
-
-    requires: [
-        'Contacts.view.contacts.addcontact.AddContact',
-    ],
-
-    xtype: 'contacts',
     
+    requires: [
+    'Contacts.view.contacts.editcontact.EditContact',
+    'Contacts.view.contacts.addcontact.AddContact',
+    
+    ],
+    
+    xtype: 'contacts',
     layout: 'card',
-
+    
     cls: 'contacts',
-
+    
     style: 'background-color: rgba(248, 248, 248, 1)',
-
+    activeItem:1,
     items: [
-        {
-            // html: 'Contacts',
-            xtype: 'addcontact'
-        },{
-            html: 'Edit contacts',
-        },{
-            html: 'Display Gridkkkkk',
-
-        }
+    {
+    html: 'Contacts',
+    xtype: 'addcontact'
+    },{
+    xtype:'editcontact'
+    },{
+    html: 'Display Gridkkkkk',
+    
+    }
     ]
-});
+    });
---
+++ packages/local/contacts/src/view/contacts/editcontact/EditContact.js
@@ -0,0 +1,194 @@
+Ext.define('Contacts.view.contacts.editcontact.EditContact',{
+    extend:'Ext.form.Panel',
+    xtype:'editcontact',
+
+    // bodyStyle: 'margin-left: 100px',
+    bodyPadding:'0 100' ,
+   
+    scrollable:true,
+    fieldDefaults: {
+        labelAlign: 'top',
+        msgTarget: 'side',
+        
+    },
+
+    items:[
+       
+        {
+            xtype:'fieldcontainer',
+            
+            layout:{
+                type:'vbox',
+                pack:'center',
+                align:'middle'
+            },    
+            items:[
+            {
+                html:'<div style="font-size:large; font-weight:bold;">Edit Contact</div>',
+                margin:'30 0 10 0 '
+            },
+            {
+                xtype: 'image',
+                src: 'resources/images/ben-knapen.png',
+                height:100,
+                width:100
+            },{
+                // xtype: 'filefield', 
+                html:"<a href='#'>edit contact</a>"
+            }]
+            
+        },
+        {
+        xtype:'fieldcontainer',
+        fieldLabel: 'Postal Address',
+        layout: 'column',
+        border:false ,
+        defaults: {
+            columnWidth: 0.25,
+            labelSeparator:'',
+            margin:'10 10 10 10'    ,
+            afterLabelTextTpl:  '<span style="color:red;font-weight:bold" data-qtip="Required">*</span>' 
+        },
+        items: [
+            {
+                xtype: 'datefield',
+                fieldLabel: 'Date',
+                name: 'first'
+            },
+            {
+    
+                    xtype:'textfield',
+                    fieldLabel: 'First Name',
+                    name: 'firstName'
+            },{
+                xtype: 'textfield',
+                fieldLabel: 'Last Name',
+                name: 'lastName'
+            },{
+                xtype: 'textfield',
+                fieldLabel: 'Mobile Number',
+                name: 'mobileNumber',
+                emptyText: '+911234567890'
+            },
+            {
+                xtype: 'textfield',
+                fieldLabel: 'Email Id',
+                name: 'email',
+                vtype: 'email'
+            },
+            {
+    
+                    xtype:'textfield',
+                    fieldLabel: 'Company Name',
+                    name: 'company'
+            },{
+                xtype: 'textfield',
+                fieldLabel: 'Title',
+                name: 'title'
+            },{
+                xtype: 'textfield',
+                fieldLabel: 'Industry',
+                name: 'industry',
+                afterLabelTextTpl: ''
+            },
+            {
+                xtype: 'textfield',
+                fieldLabel: 'Primary Buniness',
+                name: 'business',
+                afterLabelTextTpl: ''
+            },
+            {
+                 xtype:'textfield',
+                 fieldLabel: 'Website',
+                 name: 'website',
+                 afterLabelTextTpl: ''
+            },{
+                xtype: 'numberfield',
+                fieldLabel: 'No. of Employees',
+                name: 'noOfEmployees',
+                afterLabelTextTpl: ''
+            },{
+                xtype: 'textfield',
+                fieldLabel: 'Revenue',
+                name: 'revenue',
+                afterLabelTextTpl: ''
+            },
+            {
+                xtype: 'textfield',
+                fieldLabel: 'Linked In',
+                name: 'linked',
+                afterLabelTextTpl: ''
+            }
+        ]},{
+            xtype:'fieldcontainer',
+            fieldLabel: 'Address Information',
+            layout: 'column',
+            defaults: {
+                columnWidth: 0.25,  
+                margin:'10 10 10 10'  
+            },
+            items: [
+                {
+                    xtype: 'textfield',
+                    fieldLabel: 'Address',
+                    name: 'address'
+                },
+                {
+                    xtype:'textfield',
+                    fieldLabel: 'City',
+                    name: 'city'
+                },{
+                    xtype: 'textfield',
+                    fieldLabel: 'State',
+                    name: 'state'
+                },{
+                    xtype: 'combobox',
+                    fieldLabel: 'Country',
+                    name: 'country',
+                    // store: {
+                    //     type: 'country'
+                    // },
+                    valueField: 'abbr',
+                    displayField: 'country',
+                    typeAhead: true,
+                    queryMode: 'local',
+                    emptyText: 'Select a state...'
+                },
+            ]
+        },{
+            xtype:'fieldcontainer',
+            fieldLabel: 'Address Information',
+            layout: 'column',
+            border:false ,
+            defaults: {
+                columnWidth: 1,    
+                margin:'10 10 10 10'
+            },
+            items:[{
+                xtype: 'textarea',
+                hideLabel: true,
+                fieldLabel:'Notes',
+                name: 'notes',
+            }]
+        },
+        {
+            xtype:'fieldcontainer',
+            layout:{
+                type:'hbox',
+                pack:'center',
+                align:'middle'
+            },
+            defaults:{
+                xtype:'button'
+            },
+            items:[{
+                text:'Cancel'
+            },{
+                text:'Save'
+            }
+            ]
+        }
+    ],
+
+  
+});
